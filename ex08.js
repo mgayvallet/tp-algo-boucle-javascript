9db84df252b26ee8c6eb0bc843bebcbe2ef6a9b9
qwyx <mc010116@gmail.com>
2024-11-25 16:02:10 +0100

    finished 08

---
+++ ex08.js
@@ -0,0 +1,9 @@
+// ex08.js
+
+function containsElement(arr, element) {
+    return arr.includes(element);
+}
+
+// Exemples de test :
+console.log(containsElement([1, 2, 3], 2)); // Résultat attendu : true
+console.log(containsElement([4, 5, 6], 1)); // Résultat attendu : false
