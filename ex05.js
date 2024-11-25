2d5f8fabb69bf73b3a72ef07ed21ce730599c3be
qwyx <mc010116@gmail.com>
2024-11-25 15:44:18 +0100

    finished 05.js

---
+++ ex05.js
@@ -0,0 +1,15 @@
+// ex05.js
+
+function countVowels(str) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if ('aeiouy'.includes(str[i])) {
+            count++;
+        }
+    }
+    return count
+}
+
+// Exemples de test:
+console.log(countVowels("hello")); // Résultat attendu : 2
+console.log(countVowels("xyz")); // Résultat attendu : 1
